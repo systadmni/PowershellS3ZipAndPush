5650863653ef9d6bb92a49c1a0fc07715269dc0a
systadmni <systadmni@gmail.com>
2014-07-21 11:30:04 -0700

    Update PS7zip.ps1

--- PS7zip.ps1
+++ PS7zip.ps1
@@ -1,10 +1,10 @@
-$7zip-path = "C:\Program Files\7-zip\"
+$7zipPath = "C:\Program Files\7-zip\"
 
-$s_path = read.host "Enter the source directory: "
-$passw = read.host "Enter the passphrase to be used to encrypt the zipped file: "
-$dest_path = read.host "Enter the path for destination: "
-$dest_file = read.host "Enter the output file name (add .7z to the end): "
+$s_path = read-host "Enter the source directory: "
+$passw = read-host "Enter the passphrase to be used to encrypt the zipped file: "
+$dest_path = read-host "Enter the path for destination: "
+$dest_file = read-host "Enter the output file name (add .7z to the end): "
 $file_path = $dest_path + "/" + $dest_file
 
-cd $7zip-path
+cd $7zipPath
 ./7z.exe a -t7z -p"$passw" "$file_path" "$source_path"
